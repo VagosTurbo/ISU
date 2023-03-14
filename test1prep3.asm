7a0a0fc9bc2b4c4a34fdee8e45e2b7653f7665d9
Boris Semanco <semanco.boris@gmail.com>
2023-03-14 15:20:30 +0100

    ucenie na test1

--- test1prep3.asm
+++ test1prep3.asm
@@ -3,10 +3,10 @@
 section .data
 
     a db 7
-    b dd -2
-    c dw -5
-    d dw -4
-    e dw -3
+    b dd 2
+    c dw 5
+    d dw 4
+    e dw 3
     q dd 0
     r dd 0
 
@@ -15,36 +15,34 @@ CMAIN:
 	push ebp
 	mov ebp,esp
 
-;4321 -> 1342
-
     mov eax, 0xa4a3a2a1
 
-    rol eax, 16
-    rol ax, 8
+;4321 -> 4132
+
     rol eax, 8
+    rol ax, 8
+    rol eax, 16
 
+;q = (a * b + c + 38)/(13*d+e+118)
+;r = (a * b + c + 38)%(13*d+e+118)
 
-;druha uloha
-
-;q = (a * b + c -38)/(13*d+e-108)
-;r = (a * b + c -38)%(13*d+e-108)
-    mov eax, 0
-    movsx eax, byte [a]
+    movzx eax, byte [a]
     mov ebx, dword [b]
-    imul ebx
-    movsx ebx, word [c]
+    mul ebx
+    movzx ebx, word [c]
     add eax, ebx
     adc edx, 0
-    sub eax, 38
+    add eax, 38
     mov ecx, eax
 
     mov eax, 13
-    movsx ebx, word [d]
-    imul ebx
-    movsx ebx, word [e]
+    movzx ebx, word [d]
+    mul ebx
+    movzx ebx, word [e]
     add eax, ebx
     adc edx, 0
-    sub eax, 108
+    add eax, 118
+    adc edx, 0
 
     mov ebx, eax
     mov eax, ecx
@@ -52,9 +50,6 @@ CMAIN:
     div ebx
     mov [q], eax
     mov [r], edx
-
-
-
 
 	pop ebp
 	ret
